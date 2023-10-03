83bf4d15a1ea31540ce8646c2167f3e78ad23573
Derek <dereklau15@gmail.com>
2023-10-03 10:45:59 +0900

    Delete index.js tests

--- index.js
+++
@@ -1,9 +0,0 @@
-const LinkedList = require("./linkedlist.js");
-
-const list = new LinkedList();
-list.append(20);
-list.append(30);
-list.append(40);
-
-console.log(JSON.stringify(list, null, 3));
-console.log(list.toString());
